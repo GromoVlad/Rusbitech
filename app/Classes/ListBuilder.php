575a836e078596ed2ca1119f73c33b28ec2416bf
Vlad Gromov <vikkiners@yandex.ru>
2020-10-09 01:19:16 +0300

    выполнена основная часть бэкенда (без документации)

---
+++ app/Classes/ListBuilder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Classes;
+
+use App\Models\News;
+use Illuminate\Http\Request;
+
+class ListBuilder
+{
+    private $params;
+    private $list;
+
+    public function __construct(Request $request)
+    {
+        $this->params = $request;
+        $this->list = News::leftJoin('authors', 'news.author_id', '=', 'authors.id')->select('news.id', 'news.name',
+            'news.announce', 'news.date', 'authors.name as author_name');
+        $this->checkAuthor()->checkDateFrom()->checkDateTo();
+    }
+
+    public function getList()
+    {
+        return $this->list;
+    }
+
+    private function checkAuthor()
+    {
+        if($this->params['author'] > 0){
+            $this->list = $this->list->where('author_id', $this->params['author']);
+        }
+        return $this;
+    }
+
+    private function checkDateFrom()
+    {
+        if(!is_null($this->params['date_from'])){
+            $this->list = $this->list->where('date', '>=', $this->params['date_from']);
+        }
+        return $this;
+    }
+
+    private function checkDateTo()
+    {
+        if(!is_null($this->params['date_to'])){
+            $this->list = $this->list->where('date', '<=', $this->params['date_to']);
+        }
+        return $this;
+    }
+
+}
